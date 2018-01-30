f49cc346091b2b1885d18255e1b7b87f6a101708
Stefan Bambach <stefan@bambach.biz>
2018-01-30 17:34:23 +0100

    added ADVANCED_PAUSE_FEATURE and NOZZLE_PARK_FEATURE

--- firmware/marlin/Configuration_adv.h
+++ firmware/marlin/Configuration_adv.h
@@ -876,7 +876,7 @@
  * Requires NOZZLE_PARK_FEATURE.
  * This feature is required for the default FILAMENT_RUNOUT_SCRIPT.
  */
-//#define ADVANCED_PAUSE_FEATURE
+#define ADVANCED_PAUSE_FEATURE
 #if ENABLED(ADVANCED_PAUSE_FEATURE)
   #define PAUSE_PARK_RETRACT_FEEDRATE 60      // Initial retract feedrate in mm/s
   #define PAUSE_PARK_RETRACT_LENGTH 2         // Initial retract in mm

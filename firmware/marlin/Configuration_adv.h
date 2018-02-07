41a71e09116a303f94829613557c65f86df74074
Stefan Bambach <stefan@bambach.biz>
2018-02-07 17:34:25 +0100

    added SENSORLESS_HOMING support for 1.1.8

--- firmware/marlin/Configuration_adv.h
+++ firmware/marlin/Configuration_adv.h
@@ -1123,11 +1123,11 @@
    * It is advised to set X/Y_HOME_BUMP_MM to 0.
    * M914 X/Y to live tune the setting
    */
-  //#define SENSORLESS_HOMING // TMC2130 only
+  #define SENSORLESS_HOMING // TMC2130 only
 
   #if ENABLED(SENSORLESS_HOMING)
-    #define X_HOMING_SENSITIVITY  8
-    #define Y_HOMING_SENSITIVITY  8
+    #define X_HOMING_SENSITIVITY  -2
+    #define Y_HOMING_SENSITIVITY  -2
     #define X_HOME_BUMP_MM 0
     #define Y_HOME_BUMP_MM 0
   #endif
